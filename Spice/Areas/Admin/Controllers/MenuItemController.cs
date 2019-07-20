fc442e6bf67afc586731cdb9c2f6db90a74d3aa1
Sunita Bisht <sunnubisht2301@gmail.com>
2019-07-20 13:57:33 +0530

    Add MenuItems

---
+++ Spice/Areas/Admin/Controllers/MenuItemController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Spice.Data;
+using Spice.Models.ViewModels;
+
+namespace Spice.Areas.Admin.Controllers
+{
+    [Area ("Admin")]
+  
+    public class MenuItemController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+       // private readonly IHostingEnviornment _hostingEnviornment;
+       [BindProperty]
+        public MenuItemViewModel MenuItemVM { get; set; }
+        public MenuItemController(ApplicationDbContext db)
+        {
+            _db = db;
+            // _hostingEnviornment = hostingEnviornment;
+            MenuItemVM = new MenuItemViewModel()
+            {
+                Category = _db.Category,
+                MenuItem = new Models.MenuItem()
+            };
+        }
+        public async Task<IActionResult>Index()
+        {
+            var menuItem = await _db.MenuItem.ToListAsync();
+            return View(menuItem);
+        }
+
+
+        //GET-CREATE
+        public IActionResult Create()
+        {
+            return View(MenuItemVM);
+
+        }
+    }
+}
---
+++ Spice/Models/MenuItem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spice.Models
+{
+    public class MenuItem
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Discription { get; set; }
+
+        public string Spicyness { get; set; }
+        public enum ESpicy { NA = 0, NotSpicy = 1, Spicy = 2, VerySpicy = 3 }
+
+        public string Image { get; set; }
+
+        [Display(Name="Category")]
+        public int CategoryId { get; set; }
+
+        [Foreignkey("CategoryId")]
+        public virtual Category Category { get; set;}
+
+
+        [Display(Name = "SubCategory")]
+        public int SubCategoryId { get; set;}
+
+
+        [Foreignkey("SubCategoryId")]
+        public virtual SubCategory SubCategory { get; set; }
+
+
+        [Range(100,int.MaxValue, ErrorMessage="Price should be greater then Rupey{100}" )]
+        public double Price { get; set;}
+        
+
+    }
+}
